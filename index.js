4b49e8ed0c56a677c358ae9c922bea472f906f4c
Moises Mannarino <angular.moises82@gmail.com>
2022-11-30 20:29:38 -0300

    corrigiendo url home en el server

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ app.use(express.static(path.join(__dirname, '/public')))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/public/index.html'))
 })
+app.get('/home', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'))
+})
 //const openapiSpecification = swaggerJsdoc(options);
 //const swaggerUi = require('swagger-ui-express');
 //app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
