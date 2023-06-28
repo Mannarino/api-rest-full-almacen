b453b4b479ef7683eb727d6f5ea2217cfc1da8c0
moises <angular.moises82@gmail.com>
2023-06-28 05:26:12 -0300

    actualice el tema de las variables de entorno

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mongoose = require('mongoose')
+const obtenerValoresDeEntorno = require('./environment/getEnvironment.js')
+const config =obtenerValoresDeEntorno()
 
 module.exports= function db(){
-	mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PASSDB}@cluster0.wq4xmib.mongodb.net/${process.env.NAMEDB}?retryWrites=true&w=majority` ,
+	mongoose.connect(`mongodb+srv://${config.USER_DATABASE}:${config.PASSWORD_DATABASE}@cluster0.wq4xmib.mongodb.net/${process.env.NAMEDB}?retryWrites=true&w=majority` ,
 	function(){console.log('conectado a base de datos')})
 }
+
+
+//mongoose.connect(`mongodb+srv://${process.env.USERDB}:${process.env.PASSDB}@cluster0.wq4xmib.mongodb.net/${process.env.NAMEDB}?retryWrites=true&w=majority` ,
+//	function(){console.log('conectado a base de datos')})
