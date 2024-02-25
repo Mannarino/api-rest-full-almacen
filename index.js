06b3c9ab8cc38f7d36143a90e9fe7729506bed1b
moises <frontend.moises@gmail.com>
2024-02-25 17:29:16 -0300

    se saco la funcionaldiad de servir la app desde el server a traves de archivos estaticos

--- index.js
+++ index.js
@@ -8,16 +8,14 @@ var cors = require('cors')
 app.use(express.json());
 app.use(cors())
 
-app.use(express.static(path.join(__dirname, '/public')))
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'))
-})
-app.get('/home', (req, res) => {
-  res.sendFile(path.join(__dirname + '/public/index.html'))
-})
-//const openapiSpecification = swaggerJsdoc(options);
-//const swaggerUi = require('swagger-ui-express');
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(openapiSpecification));
+//app.use(express.static(path.join(__dirname, '/public')))
+//app.get('/', (req, res) => {
+//  res.sendFile(path.join(__dirname + '/public/index.html'))
+//})
+//app.get('/home', (req, res) => {
+//  res.sendFile(path.join(__dirname + '/public/index.html'))
+//})
+
 
 //conection base datos
 DBconection()
